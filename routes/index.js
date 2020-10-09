44bff3e92ac3b129e05deb15de668ce555f76d10
pinct <pinctcourneya@gmail.com>
2020-10-09 13:56:24 -0400

    Added about page. Edited projects.

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/home', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
 router.get('/projects', function(req, res, next) {
